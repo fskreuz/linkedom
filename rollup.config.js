7f2d2600b338beb41edbecb492c6ebe1472e7c75
Joseph <fskreuz@gmail.com>
2022-06-30 22:00:30 -0400

    Initial set of files.

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
+import nodeResolve from '@rollup/plugin-node-resolve'
+import { terser } from 'rollup-plugin-terser'
+
+export default {
+  input: 'index.js',
+  plugins: [
+    commonjs(),
+    nodeResolve({ preferBuiltins: true }),
+    json(),
+    terser()
+  ],
+  output: {
+    file: 'build/index.js',
+    format: 'esm'
+  }
+}
